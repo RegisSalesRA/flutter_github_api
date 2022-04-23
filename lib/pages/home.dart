0a4e273a9bb036f0ee6ee6c478656003f1cb6b7d
RegisSalesRA <regismsales@gmail.com>
2022-04-23 02:36:33 -0300

    Finish header

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -47,31 +47,64 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
           child: Column(children: [
-            Header(),
-            Expanded(
-                child: NestedScrollView(
-              controller: _scrollViewController,
-              headerSliverBuilder: (context, bool) => [
-                SliverAppBar(
-                  backgroundColor: Colors.white,
-                  bottom: TabBar(
-                    indicatorColor: CustomColors.rustyOrange,
-                    labelColor: Colors.black,
-                    unselectedLabelColor: Colors.grey,
-                    isScrollable: false,
-                    controller: _tabController,
-                    tabs: [
-                      Tab(text: "Repositorios"),
-                      Tab(text: "Starred"),
+            const Header(),
+            TabBar(
+              indicatorColor: CustomColors.rustyOrange,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey,
+              isScrollable: false,
+              controller: _tabController,
+              tabs: [
+                Tab(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Repositorios'),
+                      const SizedBox(width: 8),
+                      Container(
+                        width: 27.0,
+                        height: 20.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                          color: CustomColors.whiteTwo,
+                        ),
+                        child: Center(
+                            child: Text("100",
+                                style: TextStyle(
+                                    color: CustomColors.slateGreyTwo))),
+                      )
+                    ],
+                  ),
+                ),
+                Tab(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Starred'),
+                      const SizedBox(width: 8),
+                      Container(
+                        width: 27.0,
+                        height: 20.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                          color: CustomColors.whiteTwo,
+                        ),
+                        child: Center(
+                            child: Text("100",
+                                style: TextStyle(
+                                    color: CustomColors.slateGreyTwo))),
+                      )
                     ],
                   ),
                 ),
               ],
-              body: TabBarView(
+            ),
+            Expanded(
+              child: TabBarView(
                 controller: _tabController,
                 children: [PageOne(), PageTwo()],
               ),
-            ))
+            )
           ]),
         ));
   }
